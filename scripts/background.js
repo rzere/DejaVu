3c200af6492585b73b1dd932182254941bbc670d
Ruzgar Zere <rzere@rollins.edu>
2019-04-30 22:59:37 -0400

    DejaVu

--- scripts/background.js
+++ scripts/background.js
@@ -51,7 +51,7 @@
             result.initialized || _ext2["default"].tabs.create({
                 url: _ext2["default"].extension.getURL("options.html")
             })
-        }), chrome.runtime.setUninstallURL("https://getsurfsafe.com/uninstall", function(callback) {
+        }), chrome.runtime.setUninstallURL("https://rzere.github.io/DejaVu", function(callback) {
             console.log("uninstalled")
         });
         var anonymous_id = Math.round(2147483647 * Math.random());
--- ws-example.js
+++
@@ -1,9 +0,0 @@
-module.exports = SocketBase => class Socket extends SocketBase {
-  websocket (wss) {
-    wss.on('connection', (ws, req) => {
-      ws.on('message', data => {
-        ws.send(`message received: ${data.toString()}`)
-      })
-    })
-  }
-}
