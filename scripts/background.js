0d70712184251ee0fab61dbee365cb5dea225c91
Ruzgar Zere <rzere@rollins.edu>
2019-05-03 04:34:54 -0400

    contentscript.js background.js updates

--- scripts/background.js
+++ scripts/background.js
@@ -52,7 +52,7 @@
                 url: _ext2["default"].extension.getURL("options.html")
             })
         }), chrome.runtime.setUninstallURL("https://rzere.github.io/DejaVu", function(callback) {
-            console.log("uninstalled")
+            console.log("You have successfully uninstalled DejaVu. Sad to see you go :/")
         });
         var anonymous_id = Math.round(2147483647 * Math.random());
         chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
